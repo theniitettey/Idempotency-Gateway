ba06260536dcace82be134942137113a7cab99e4
michaelperryjnr <michaelperryt97@gmail.com>
2026-02-13 13:54:40 +0000

    added pino for logging

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 const CONFIG = {
     PORT: process.env.PORT || 3000,
+    LOG_LEVEL: process.env.LOG_LEVEL || "info",
     ENV: process.env.NODE_ENV || "development",
     ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS || "*",
 }
